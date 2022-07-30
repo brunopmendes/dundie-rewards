c4cdad43a28e3f6422db7c37e61a404d00cb16f1
brunopmendes <brupmendes@icloud.com>
2022-07-30 13:21:42 -0300

    turned to installable binary #9

---
+++ setup.py
@@ -0,0 +1,15 @@
+# setuptools
+from setuptools import setup, find_packages
+
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Reward Point System for Dunder Mifflin",
+    packages=find_packages(), #tudo com __init__ é considerado um pacote
+    entry_points={
+        "console_scripts": [
+            "dundie = dundie.__main__:main"
+        ]
+    }
+)
